65f90e2a1b1689c25567a68adb71f2e46e5ebaa1
adam <kaczmarek@oskarwegner.pl>
2018-07-18 12:20:47 +0200

    Generate/prepare assets json and scss script.

---
+++ prepare-assets.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+let assets = {};
+let scss = [];
+
+function walkSync(dir, filelist = {}) {
+
+  fs.readdirSync(dir).forEach(file => {
+
+    const dirFile = path.join(dir, file);
+    const relativePath = dirFile.replace('src', '');
+
+    const assetObject = {
+      path: relativePath + '?' + fs.statSync(dirFile).mtime.getTime(),
+      group: dir.split('/')[2]
+    };
+
+    try {
+      walkSync(dirFile, filelist);
+    }
+    catch (err) {
+
+      if (err.code === 'ENOTDIR' || err.code === 'EBUSY') {
+
+        assets[dirFile] = assetObject.path;
+
+        let fileFiltered = relativePath.replace(/\s|[/.]/g, "-");
+        scss.push(`$img-${fileFiltered}: "${assetObject.path}";`);
+
+      }
+      else throw err;
+
+    }
+  });
+  return filelist;
+}
+
+walkSync('./src/assets/');
+
+fs.writeFileSync('./src/assets.json', JSON.stringify(assets), 'utf8');
+fs.writeFileSync('./src/assets.scss', scss.join("\n"), 'utf8');
+
+
