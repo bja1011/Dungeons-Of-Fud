5fda9879b554c51a00b5c110885ab49dd005e3bf
adam <kaczmarek@oskarwegner.pl>
2018-07-18 14:24:54 +0200

    Game class extended with gameService property.

--- src/app/game/components/play-game/play-game.component.ts
+++ src/app/game/components/play-game/play-game.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import 'phaser';
 import {PreloadScene} from '../../scenes/Preload.scene';
 import {GameService} from '../../providers/game.service';
+import {AssetsService} from '../../../providers/assets.service';
 
 @Component({
   selector: 'app-play-game',
@@ -10,25 +11,36 @@ import {GameService} from '../../providers/game.service';
 })
 export class PlayGameComponent implements OnInit {
 
-  constructor(private gameService: GameService) {
+  constructor(private gameService: GameService,
+              private assetsService: AssetsService,) {
   }
 
   ngOnInit() {
-    this.gameService.game = new Phaser.Game({
-      width: innerWidth,
-      height: innerHeight,
-      type: Phaser.WEBGL,
-      parent: 'game',
-      disableContextMenu: true,
-      scene: [PreloadScene],
-      physics: {
-        default: 'arcade',
-        arcade: {
-          debug: false,
-          gravity: {y: 0}
+    this.gameService.game = new MyGame({
+        width: innerWidth,
+        height: innerHeight,
+        type: Phaser.WEBGL,
+        parent: 'game',
+        disableContextMenu: true,
+        scene: [PreloadScene],
+        physics: {
+          default: 'arcade',
+          arcade: {
+            debug: false,
+            gravity: {y: 0}
+          }
         }
-      }
-    });
+      },
+      this.gameService);
   }
 
+}
+
+class MyGame extends Phaser.Game {
+  gameService: GameService;
+
+  constructor(gameConfig: GameConfig, gameService: GameService) {
+    super(gameConfig);
+    this.gameService = gameService;
+  }
 }
