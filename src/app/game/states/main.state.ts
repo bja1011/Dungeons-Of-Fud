d2e99ed3f791c3c4ea5bbbf28835963ee067b579
Adam Kaczmarek <bja1011@gmail.com>
2018-11-12 23:47:44 +0100

    read pixel from bitmapdata

--- src/app/game/states/main.state.ts
+++ src/app/game/states/main.state.ts
@@ -5,21 +5,23 @@ export default class Title extends Phaser.State {
     this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
     this.game.scale.pageAlignVertically = true;
 
-    const sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'building');
-    sprite.anchor.setTo(1, 0.5);
+    const sprite = this.game.add.sprite(0, 0, 'building');
+    sprite.anchor.setTo(0, 0);
 
     const bmd = this.game.add.bitmapData(sprite.width, sprite.height);
-    bmd.addToWorld(this.game.world.centerX, this.game.world.centerY);
     bmd.draw(this.game.make.sprite(0, 0, 'building'));
+    bmd.update();
 
     for (let y = 0; y < bmd.height; y++)
       for (let x = 0; x < bmd.width; x++) {
         const pix = bmd.getPixel(x, y);
         // console.log(x,y)
-        pix.a && console.log(pix);
       }
 
+
+    bmd.addToWorld(sprite.width + 10, 0);
 
     // this.game.camera.flash(0x000000, 1000);
   }
 }
+
